6fc0e20876d63ceaa43f1f718c20e5e807478174
jester350 <jester350@gmail.com>
2023-12-10 16:04:36 +0000

    ass

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var assRouter = require('./routes/ass');
 
 var app = express();
 
@@ -21,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/ass', assRouter)
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
