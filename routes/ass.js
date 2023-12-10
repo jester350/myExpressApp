ceb0c75873e61791cfc2ed810b0cea2487f117dc
jester350 <jester350@gmail.com>
2023-12-10 16:15:59 +0000

    nass

--- routes/ass.js
+++ routes/ass.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('ass', { title: 'judys hot ass' });
+  res.render('ass', { title: 'hot ass' });
 });
 
 module.exports = router;
